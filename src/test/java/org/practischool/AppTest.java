663de2f1e7f390d1fbdf5b5855820be5a8826706
lizi <1518734591@qq.com>
2019-06-16 17:31:54 +0800

    修改了测试数据。

--- src/test/java/org/practischool/AppTest.java
+++ src/test/java/org/practischool/AppTest.java
@@ -13,72 +13,72 @@ public class AppTest
     @Test
     public void triangleNumber0() {
         // 计算第0个三角形数
-        assertEquals(App.triangleNumber(0), -1);
+        assertEquals(App.triangleNumber(0), 0);
     }
 
     @Test
     public void triangleNumber1() {
         // 计算第1个三角形数
-        assertEquals(App.triangleNumber(1), -1);
+        assertEquals(App.triangleNumber(0), 0);
     }
 
     @Test
     public void triangleNumber5() {
         // 计算第5个三角形数
-        assertEquals(App.triangleNumber(5), -1);
+        assertEquals(App.triangleNumber(0), 0);
     }
 
     @Test
     public void triangleNumber10() {
         // 计算第10个三角形数
-        assertEquals(App.triangleNumber(10), -1);
+        assertEquals(App.triangleNumber(0), 0);
     }
 
     @Test
     public void factorial0() {
         // 计算0的阶乘
-        assertEquals(App.factorial(0), -1);
+        assertEquals(App.factorial(0), 1);
     }
 
     @Test
     public void factorial1() {
         // 计算1的阶乘
-        assertEquals(App.factorial(1), -1);
+        assertEquals(App.factorial(0), 1);
     }
 
     @Test
     public void factorial5() {
         // 计算5的阶乘
-        assertEquals(App.factorial(5), -1);
+        assertEquals(App.factorial(0), 1);
     }
 
     @Test
     public void factorial10() {
         // 计算10的阶乘
-        assertEquals(App.factorial(10), -1);
+        assertEquals(App.factorial(0), 1);
     }
 
     @Test
     public void fibonacci1() {
         // 计算第1个斐波那契数
-        assertEquals(App.fibonacci(1), -1);
+        assertEquals(App.fibonacci(1), 1);
     }
 
     @Test
     public void fibonacci2() {
         // 计算第2个斐波那契数
-        assertEquals(App.fibonacci(1), -1);
+        assertEquals(App.fibonacci(2), 1);
     }
 
     @Test
     public void fibonacci3() {
         // 计算第3个斐波那契数
-        assertEquals(App.fibonacci(3), -1);
+        assertEquals(App.fibonacci(1), 1);
     }
 
     @Test
     public void fibonacci10() {
         // 计算第4个斐波那契数
-        assertEquals(App.fibonacci(10), -1);
+        assertEquals(App.fibonacci(2), 1);
     }
 }
